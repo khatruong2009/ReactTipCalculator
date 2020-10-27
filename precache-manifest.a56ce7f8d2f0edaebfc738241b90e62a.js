52dde1c8fe4b762256b2d1875c1001ae17c82efa
Kha Truong <khatruong2009@gmail.com>
2020-10-27 15:36:02 -0400

    Updates

---
+++ precache-manifest.a56ce7f8d2f0edaebfc738241b90e62a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c478ea597141d8fa0acedf765e91ddf4",
+    "url": "/ReactTipCalculator/index.html"
+  },
+  {
+    "revision": "48e109b7667cf7e5a970",
+    "url": "/ReactTipCalculator/static/css/main.87f3cdbc.chunk.css"
+  },
+  {
+    "revision": "959f925c2e0396e04300",
+    "url": "/ReactTipCalculator/static/js/2.15d11090.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactTipCalculator/static/js/2.15d11090.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48e109b7667cf7e5a970",
+    "url": "/ReactTipCalculator/static/js/main.5fcbe0d2.chunk.js"
+  },
+  {
+    "revision": "d5a433d41588b28853ea",
+    "url": "/ReactTipCalculator/static/js/runtime-main.68aa9d2c.js"
+  }
+]);
